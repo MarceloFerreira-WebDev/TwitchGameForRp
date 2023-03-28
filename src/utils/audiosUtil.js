970179b7a866f26b1fec95ab451ca9ca2519da72
Marcelo Ferreira <marceloferreira.ihu72@gmail.com>
2023-03-28 14:40:34 -0300

    ajustes dos audios

--- src/utils/audiosUtil.js
+++ src/utils/audiosUtil.js
@@ -1,12 +1,11 @@
-import Q1 from '../audios/Q1.ogg';
-import Q2 from '../audios/Q2.ogg';
-import Q3 from '../audios/Q3.ogg';
-import Q4 from '../audios/Q4.ogg';
-import exitQ1 from '../audios/exitQ1.ogg';
-import exitQ2 from '../audios/exitQ2.ogg';
-import exitQ3 from '../audios/exitQ3.ogg';
-import exitQ4 from '../audios/exitQ4.ogg';
-
+const Q1 = 'https://static.wikia.nocookie.net/leagueoflegends/images/2/20/Twitch_Original_Q_1.ogg';
+const Q2 = 'https://static.wikia.nocookie.net/leagueoflegends/images/1/18/Twitch_Original_Q_2.ogg';
+const Q3 = 'https://static.wikia.nocookie.net/leagueoflegends/images/7/77/Twitch_Original_Q_3.ogg';
+const Q4 = 'https://static.wikia.nocookie.net/leagueoflegends/images/e/ec/Twitch_Original_Q_0.ogg';
+const exitQ1 = 'https://static.wikia.nocookie.net/leagueoflegends/images/2/2d/Twitch_Original_QBuff_OnBuffActivate_0.ogg';
+const exitQ2 = 'https://static.wikia.nocookie.net/leagueoflegends/images/b/b4/Twitch_Original_QBuff_OnBuffActivate_1.ogg';
+const exitQ3 = 'https://static.wikia.nocookie.net/leagueoflegends/images/6/62/Twitch_Original_QBuff_OnBuffActivate_3.ogg';
+const exitQ4 = 'https://static.wikia.nocookie.net/leagueoflegends/images/1/1e/Twitch_Original_QBuff_OnBuffActivate_2.ogg';
 const qList = [new Audio(Q1), new Audio(Q2), new Audio(Q3), new Audio(Q4)];
 const exitQList = [new Audio(exitQ1), new Audio(exitQ2), new Audio(exitQ3), new Audio(exitQ4)];
 
