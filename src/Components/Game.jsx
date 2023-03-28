19fa0215ac6d747619b0d0d004fcb174e1a03ca8
Marcelo Ferreira <marceloferreira.ihu72@gmail.com>
2023-03-28 15:28:58 -0300

    retira url da imagem do twitch

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import audios from '../utils/audiosUtil';
+import twitch from '../images/pngegg.png';
 
 function Game() {
   const [gameStart, setGameStart] = useState('paused');
@@ -22,7 +23,7 @@ function Game() {
     setGameStart('paused');
     audios.stopAudio();
     audios.randomExitQAudio().play();
-  };  
+  };
 
   return (
     <div style={ { width: '1024px', height: '768px', border: '1px solid black' } }>
@@ -30,9 +31,9 @@ function Game() {
       <div>
         <div className={ directions[0] } style={{ animation: gameStart, width: '100px', marginLeft: '0px !important' }}>
           <img
-            src="https://static.wikia.nocookie.net/leagueoflegends/images/c/c5/Twitch_Dragonslayer_%28Base%29.png" 
+            src={twitch} 
             className={ gameStart === '' ? directions[1] : '' }
-            style={{ animation: gameStart, width: '200px' }}
+            style={{ animation: gameStart, height: '200px' }}
             onClick={handleTwitchClick}
           />
         </div>
